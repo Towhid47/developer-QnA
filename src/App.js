a5abd154626c5118a6e05430aa27350cdc17a0d9
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-10-16 15:48:03 +0600

    React Bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
           <h1>Hello Developer</h1>
+          <Button>Be Strong</Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// import stylesheet of React BootStrap
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
