4ae0ec32114bde069c1534fb353a815fe3bca784
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-10-16 16:42:51 +0600

    Navigationbar component created

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navigationbar from '../components/Navigationbar/Navigationbar';
 
 
 const Root = () => {
     return (
         <div>
             <h1>This is Root Section</h1>
+            <Navigationbar></Navigationbar>
         </div>
     );
 };
