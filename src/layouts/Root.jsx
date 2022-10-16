f580984e5e675cbb236a3aacb699d24bfec75034
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-10-16 19:31:57 +0600

    in Root.jsx ,all the children routes components are being enabled by <outlet/>

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import Blog from '../components/Blog/Blog';
-import Home from '../components/Home/Home';
+import { Outlet } from 'react-router-dom';
 import Navigationbar from '../components/Navigationbar/Navigationbar';
-import Statistics from '../components/Statistics/Statistics';
-import Topics from '../components/Topics/Topics';
 
 
 const Root = () => {
     return (
         <div>
             <Navigationbar></Navigationbar>
-            <Home></Home>
-            <Topics></Topics>
-            <Statistics></Statistics>
-            <Blog></Blog>
+            <Outlet></Outlet>
         </div>
     );
 };
