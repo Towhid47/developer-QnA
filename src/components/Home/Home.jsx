8a7c9e08e332e8a3ff3c4c15b6b64348a428dc1f
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-10-17 00:19:51 +0600

    heading button added in Home Component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,14 +4,17 @@ import React from 'react';
 import Exam from "../../Exam.json";
 
 import Lottie from "lottie-react";
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div className='d-flex align-items-center'>
-            <header className='ms-5'>
+        <div className='d-flex flex-column flex-lg-row align-items-lg-center'>
+            <header className='mx-5'>
                 <h1>Welcome to <span className='text-info'>Developer QnA!</span></h1>
                 <h3>Test yourself and Enhance your Knowledge in Web Development</h3>
                 <p>We provide a tons of Questions about Web Development and update it every week.Here, you can choose web development Topics and can Test yourself. Which will help you to improve your knowledge and make you better than past. Several Topics are included such as : HTML, CSS , JavaScript , Git , Github , REACT JS and many more. This can also build your confidence for the Job Interview. So, what are you waiting For ? Let's Begin to Strengthen your brain.</p>
+                <Link to='/topics'><Button>Start Quiz</Button></Link>
             </header>
           
           {/* this div section is for Animation */}
