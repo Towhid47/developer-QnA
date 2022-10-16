69e4668bc92c8366bae8936bf37f130002315e18
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-10-16 22:37:48 +0600

    Headline added in Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>This is Home route</h1>
+            <header>
+                <h1>Welcome to <span className='text-info'>Developer QnA!</span></h1>
+                <h3>Test yourself and Enhance your Knowledge in Web Development</h3>
+                <p>We provide a tons of Questions about Web Development and update it every week.Here, you can choose web development Topics and can Test yourself. Which will help you to improve your knowledge and make you better than past. Several Topics are included such as : HTML, CSS , JavaScript , Git , Github , REACT JS and many more. This can also build your confidence for the Job Interview. So, what are you waiting For ? Let's Begin to Strengthen your brain.</p>
+            </header>
+          
+          {/* this div section is for Animation */}
+            <div>
+                 
+            </div>
         </div>
     );
 };
