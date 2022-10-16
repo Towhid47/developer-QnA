f7303273025a702410a2911d6c59f719b719ecb3
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-10-16 23:42:28 +0600

    Lottie Animation installed and Added in Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+//import Lottie Animation
+import Exam from "../../Exam.json";
+
+import Lottie from "lottie-react";
+
 const Home = () => {
     return (
-        <div>
-            <header>
+        <div className='d-flex align-items-center'>
+            <header className='ms-5'>
                 <h1>Welcome to <span className='text-info'>Developer QnA!</span></h1>
                 <h3>Test yourself and Enhance your Knowledge in Web Development</h3>
                 <p>We provide a tons of Questions about Web Development and update it every week.Here, you can choose web development Topics and can Test yourself. Which will help you to improve your knowledge and make you better than past. Several Topics are included such as : HTML, CSS , JavaScript , Git , Github , REACT JS and many more. This can also build your confidence for the Job Interview. So, what are you waiting For ? Let's Begin to Strengthen your brain.</p>
@@ -11,7 +16,7 @@ const Home = () => {
           
           {/* this div section is for Animation */}
             <div>
-                 
+                 <Lottie animationData={Exam}></Lottie>
             </div>
         </div>
     );
