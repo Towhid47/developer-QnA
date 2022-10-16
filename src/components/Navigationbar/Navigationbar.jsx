15abeb976bfca45f9225205cd9e716a176913b78
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-10-16 16:56:51 +0600

    Navbar theme is darkenedby using variant=dark

--- src/components/Navigationbar/Navigationbar.jsx
+++ src/components/Navigationbar/Navigationbar.jsx
@@ -4,30 +4,22 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 const Navigationbar = () => {
     return (
         <div>
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="dark" expand="lg" variant='dark'>
                     <Container>
-                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                        <Navbar.Brand href="/home">Developer QnA</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                            </NavDropdown>
+                            <Nav.Link href="/home">Home</Nav.Link>
+                            <Nav.Link href="/topics">Topics</Nav.Link>
+                            <Nav.Link href="/statistics">Statistics</Nav.Link>
+                            <Nav.Link href="/blog">Blog</Nav.Link>
+                            
                         </Nav>
                         </Navbar.Collapse>
                     </Container>
