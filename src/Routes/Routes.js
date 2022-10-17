b96aa421255c032da1502317a3de69a8b3c9c08e
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-10-18 02:59:46 +0600

    Line Chart Added in Statistics Route

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -14,7 +14,7 @@ export const router = createBrowserRouter([
         { path: '/' , element: <Home></Home> , loader: ()=> fetch('https://openapi.programming-hero.com/api/quiz') },
         { path: '/home' , element: <Home></Home> , loader: ()=> fetch('https://openapi.programming-hero.com/api/quiz') },
         { path: '/topics' , element: <Topics></Topics> , loader: ()=> fetch('https://openapi.programming-hero.com/api/quiz') },
-        { path: '/statistics', element: <Statistics></Statistics>},
+        { path: '/statistics', element: <Statistics></Statistics> ,  loader: () => fetch('https://openapi.programming-hero.com/api/quiz')},
         { path: '/blog' , element: <Blog></Blog>},
         { path: '/quiz/:id' , element: <Quiz></Quiz> , loader: ({params}) => fetch(`https://openapi.programming-hero.com/api/quiz/${params.id}`) }
     ]
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
 
 const Statistics = () => {
+     const loadData = useLoaderData();
+     console.log(loadData.data); 
+
     return (
         <div>
-            <h1>This is Statistics Route</h1>
+           {/* Simple Line Chart */}
+            <div>
+                <LineChart width={800} height={400} data={loadData.data} margin={{ top: 10, right: 0, bottom: 0, left: 0 }}>
+                    <Line type="monotone" dataKey="total" stroke="#00FFFF" />
+                    <XAxis dataKey="name"/>
+                    <YAxis />
+                    <Tooltip />
+                </LineChart>
+            </div>
         </div>
     );
 };
